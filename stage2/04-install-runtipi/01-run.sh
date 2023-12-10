20e8b8d6b97479fa9c07dbeb609af569eb34ad6b
Stavros <steveiliop56@gmail.com>
2023-12-10 12:16:23 +0200

    refactor(scripts): update runtipi install to skip service

--- stage2/04-install-runtipi/01-run.sh
+++ stage2/04-install-runtipi/01-run.sh
@@ -34,33 +34,41 @@ chmod +x "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/runtipi/runtipi-cli"
 echo "Setting permissions..."
 
 on_chroot << EOF
-chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/runtipi/
+chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/runtipi/
 EOF
+
+echo "Starting tipi..."
+
+./${ROOTFS_DIR}/home/${FIRST_USER_NAME}/runtipi/runtipi-cli start
 
 # Enable runtipi on boot 
 
-echo "Install service..."
+if [[ "$VERSION" < "v2.2.0" ]]; then # Backwards Compatability
+    echo "Install service..."
 
-install -v files/tipi.service "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
+    install -v files/tipi.service "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
 
-echo "Make tweaks..."
+    echo "Make tweaks..."
 
-sed -i "s|WorkingDirectory=/home/username/runtipi/|WorkingDirectory=/home/${FIRST_USER_NAME}/runtipi/|" "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
-sed -i "s|ExecStart=/home/username/runtipi/runtipi-cli start|ExecStart=/home/${FIRST_USER_NAME}/runtipi/runtipi-cli start|" "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
-sed -i "s|ExecStop=/home/username/runtipi/runtipi-cli stop|ExecStop=/home/${FIRST_USER_NAME}/runtipi/runtipi-cli stop|" "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
+    sed -i "s|WorkingDirectory=/home/username/runtipi/|WorkingDirectory=/home/${FIRST_USER_NAME}/runtipi/|" "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
+    sed -i "s|ExecStart=/home/username/runtipi/runtipi-cli start|ExecStart=/home/${FIRST_USER_NAME}/runtipi/runtipi-cli start|" "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
+    sed -i "s|ExecStop=/home/username/runtipi/runtipi-cli stop|ExecStop=/home/${FIRST_USER_NAME}/runtipi/runtipi-cli stop|" "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
 
-chmod -x "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
+    chmod -x "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
 
-echo "Enable runtipi on boot..."
+    echo "Enable runtipi on boot..."
 
-on_chroot << EOF
-systemctl enable tipi
+    on_chroot << EOF
+    systemctl enable tipi
 EOF
+else
+    echo "Skipping service install..."
+fi
 
 # Verification
 
-echo "Verify... (debug)"
+# echo "Verify... (debug)"
 
-ls -la "${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
-ls -la "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/runtipi"
-cat "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
+# ls -la "${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
+# ls -la "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/runtipi"
+# cat "${ROOTFS_DIR}/etc/systemd/system/tipi.service"
